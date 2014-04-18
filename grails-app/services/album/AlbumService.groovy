1484cb64d81a9f4ef44a5bf79a58dc3421613c04
huangsiwei <huangsiwei@admaster.com.cn>
2014-04-18 16:39:26 +0800

    set timeout time 10000 in Album filter

--- grails-app/services/album/AlbumService.groovy
+++ grails-app/services/album/AlbumService.groovy
@@ -6,7 +6,7 @@ import org.jsoup.nodes.Document
 class AlbumService {
 
     def getPhotoPageList(String albumURL) {
-        Document document = Jsoup.connect(albumURL).get()
+        Document document = Jsoup.connect(albumURL).timeout(10000).get()
         def albumName = document.select("#content .info h1").html()
         def photoPageRawList = document.select(".photolst .photo_wrap a")
         def photoPageList = []
@@ -23,13 +23,13 @@ class AlbumService {
         def userList = []
         def douMailURLList = []
         photoPageList.each { pohotPage ->
-            Document document = Jsoup.connect(pohotPage).get()
+            Document document = Jsoup.connect(pohotPage).timeout(10000).get()
             document.select(".content .author a").each { userRawURL ->
                 userList << userRawURL.attr("href")
             }
         }
         userList.each { String userURL ->
-            Document document = Jsoup.connect(userURL).get()
+            Document document = Jsoup.connect(userURL).timeout(10000).get()
             def userPhotoURL = document.select("#profile .infobox .bd img").attr("src").split("icon/ul")
             if (userPhotoURL.size()==2) {
                 println(userPhotoURL)
