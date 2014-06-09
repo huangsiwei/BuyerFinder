ff7b34c94a511513c1b33628741a260895bec30d
huangsiwei <huangsiwei@admaster.com.cn>
2014-06-09 15:02:14 +0800

    remove println log

--- grails-app/services/shopwatcher/WatcherService.groovy
+++ grails-app/services/shopwatcher/WatcherService.groovy
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Document
 class WatcherService {
 
     def findShopProducts(String shopHotSellerDescUrl){
-//        pageNum=1
         Document document = Jsoup.connect(shopHotSellerDescUrl).get()
         def productNum = document.select(".search-result span").html() as Integer
         def pageNum = Math.ceil(productNum/24) as Integer
@@ -19,8 +18,6 @@ class WatcherService {
         Set productList = []
         productListUrlList.each {String productPage ->
             sleep(3000)
-            println(productPage)
-            println("=====================")
             Document productPageDoc = Jsoup.connect(productPage).get()
             def productBlockList = productPageDoc.select(".item")
             productBlockList.each { productBlock ->
@@ -28,9 +25,6 @@ class WatcherService {
                 println(productURL)
                 productList << productURL
             }
-            println("=====================")
-            println(productList.size())
-            println("=====================")
         }
         return productList
     }
