162ef0f893c6eb1d4a06c93d22bc83c9d214aa4d
huangsiwei <huangsiwei@admaster.com.cn>
2014-06-27 11:48:59 +0800

    fix bug caused by 0 sales count product

--- grails-app/controllers/shopwatcher/taobao/DailySalesController.groovy
+++ grails-app/controllers/shopwatcher/taobao/DailySalesController.groovy
@@ -10,14 +10,16 @@ class DailySalesController {
     CommonService commonService
     WatcherService watcherService
 
-    def index() {}
+    def index() {
+
+    }
 
     def todaySales() {
         def toCrawlerUrl = "http://nv-er.taobao.com/search.htm?v=1&orderType=hotsell_desc"
     }
 
     def test() {
-        def startDate = commonService.getYesterDayTime(23,0,0)
+        def startDate = commonService.getYesterDayTime(16,0,0)
         def hotSellUrl = "http://nv-er.taobao.com/search.htm?spm=a1z10.1.0.0.sJSomD&search=y&orderType=hotsell_desc&v=1"
         def productList = watcherService.findShopProducts(hotSellUrl)
         println(productList)
@@ -26,14 +28,12 @@ class DailySalesController {
             def salesInfoList = []
             List resultList = dailyCountService.findProductTodaySalesInfo(productUrl,1,salesInfoList,startDate)
             if (resultList.size() > 0) {
-                println("URL:" + productUrl)
                 println("======================================")
                 println(productName)
                 println(">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<")
                 resultList.each { salesInfo ->
                     println (salesInfo["buyer"] + "||" + salesInfo["price"] + "||" + salesInfo["salesAmount"] + "||" + salesInfo["skuWarp"] + "||" + salesInfo["salesDate"])
                 }
-                println("======================================")
             }
         }
         println("end!!!")
--- grails-app/services/shopwatcher/DailyCountService.groovy
+++ grails-app/services/shopwatcher/DailyCountService.groovy
@@ -23,31 +23,35 @@ class DailyCountService {
     }
 
     def findProductTodaySalesInfo(String productUrl,int pageNum,List salesInfoList,Date startDate) {
-        commonService.sleepRandomTime(4000,6000)
+//        commonService.sleepRandomTime(4000,6000)
         Document document = Jsoup.connect(productUrl).timeout(10000).get()
         def item_id = document.select("#J_Pine").attr("data-itemid")
         def seller_num_id = document.select("#J_Pine").attr("data-sellerid")
         def sbn = document.select("#J_listBuyerOnView").attr("data-api").split("sbn=")[1].split("&")[0]
         String salesInfoURL = basicUrl + "&bid_page=" + pageNum + "&page_size=15" + "&item_id=" + item_id + "&seller_num_id=" + seller_num_id + "&sbn=" + sbn + "&callback=Hub.data.records_reload&qq-pf-to=pcqq.c2c"
         def nextPageFlag = true
-        commonService.sleepRandomTime(4000,6000)
+//        commonService.sleepRandomTime(4000,6000)
         String result = new URL(salesInfoURL).getText('GBK',connectTimeout:10000).replace('Hub.data.records_reload({html:"', '').replace(' ",type:"list"})', "").replaceAll(/\\(['"])/, '$1')
         Document resultDoc = Jsoup.parse(result)
         def buyerList = resultDoc.select("tr [class=tb-sellnick]")
-        buyerList.each { buyer ->
-            def salesRef = buyer.parent().parent()
-            def buyerName = buyer.html().replace('<span class="tb-anonymous">**</span>', '**')
-            def price = salesRef.select("td [class=tb-rmb-num]").html()
-            def salesAmount = salesRef.select(".tb-amount").html()
-            def skuWarp = salesRef.select('.tb-sku p')[0].html()
-            def salesDateStr = salesRef.select(".tb-start").html()
-            def productName
-            def salesInfo = ["buyer": buyerName, "price": price, "salesAmount": salesAmount, "skuWarp": skuWarp, "salesDate": salesDateStr]
-            Date salesDate = Date.parse("yyyy-MM-dd HH:mm:ss", salesDateStr)
-            if (salesDate > startDate) {
-                salesInfoList << salesInfo
-            } else {
-                nextPageFlag = false
+        if (buyerList.size() == 0){
+            nextPageFlag = false
+        } else {
+            buyerList.each { buyer ->
+                def salesRef = buyer.parent().parent()
+                def buyerName = buyer.html().replace('<span class="tb-anonymous">**</span>', '**')
+                def price = salesRef.select("td [class=tb-rmb-num]").html()
+                def priceInt = price as float
+                def salesAmount = salesRef.select(".tb-amount").html()
+                def skuWarp = salesRef.select('.tb-sku p')[0].html()
+                def salesDateStr = salesRef.select(".tb-start").html()
+                def salesInfo = ["buyer": buyerName, "price": price, "salesAmount": salesAmount, "skuWarp": skuWarp, "salesDate": salesDateStr]
+                Date salesDate = Date.parse("yyyy-MM-dd HH:mm:ss", salesDateStr)
+                if (salesDate > startDate) {
+                    salesInfoList << salesInfo
+                } else {
+                    nextPageFlag = false
+                }
             }
         }
         if (!nextPageFlag) {
